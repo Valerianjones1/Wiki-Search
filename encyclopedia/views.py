34767d910c9fa9719d396711418939b5a269c1fc
Valerianjones1 <jack.wikom@gmail.com>
2022-09-23 14:48:55 +0300

    inital commit

---
+++ encyclopedia/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render, redirect
+
+import markdown2 as md
+from . import util
+from bs4 import BeautifulSoup
+
+
+def index(request):
+    return render(request, "encyclopedia/index.html", {
+        "entries": util.list_entries()
+    })
+
+
+def title(request, entry):
+    return render(request, "./encyclopedia/title.html", {"entry": entry.lower().capitalize(), "content": md.markdown(util.get_entry(entry))})
+
+
+def search(request, query):
+    print("HERE")
+    entries = util.list_entries()
+    for ent in entries:
+        if query.lower() == entries.lower():
+            return redirect(f"/{ent}")
+        elif query in ent:
+            return render(request, "./encyclopedia/search.html", {"query": query, "entry": ent, "entries": util.list_entries()})
+    return render(request, "./encyclopedia/search.html", {"query": query, "entries": util.list_entries()})
