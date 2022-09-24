9f5ffbec0bd736582c883b4ea6fe99e792434699
Valerianjones1 <jack.wikom@gmail.com>
2022-09-24 17:39:19 +0300

    Search issue

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -6,21 +6,41 @@ from bs4 import BeautifulSoup
 
 
 def index(request):
-    return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
-    })
+    if request.method == "GET":
+        query_dict = request.GET
+        query = query_dict.get('q')
+        print(query, "IAMHERE")
+        if not query:
+            return render(request, "encyclopedia/index.html", {
+                "entries": util.list_entries()
+            })
+        else:
+            entries = util.list_entries()
+            for ent in entries:
+                if query.lower() == ent.lower():
+                    return redirect(f"http://127.0.0.1:8000/{ent}")
+                elif query in ent:
+                    return render(request, "./encyclopedia/search.html", {"query": query, "entry": ent, "entries": entries})
 
 
 def title(request, entry):
-    return render(request, "./encyclopedia/title.html", {"entry": entry.lower().capitalize(), "content": md.markdown(util.get_entry(entry))})
+    content = md.markdown(util.get_entry(entry))
+    entry = entry.lower().capitalize()
+    return render(request, "./encyclopedia/title.html", {"entry": entry, "content": content})
 
 
-def search(request, query):
-
-    entries = util.list_entries()
-    for ent in entries:
-        if query.lower() == entries.lower():
-            return redirect(f"/{ent}")
-        elif query in ent:
-            return render(request, "./encyclopedia/search.html", {"query": query, "entry": ent, "entries": util.list_entries()})
-    return render(request, "./encyclopedia/search.html", {"query": query, "entries": util.list_entries()})
+def search(request):
+    query_dict = request.GET
+    query = query_dict.get('q')
+    print(query, query_dict, "IAMHERE")
+    if not query:
+        return render(request, "encyclopedia/index.html", {
+            "entries": util.list_entries()
+        })
+    else:
+        entries = util.list_entries()
+        for ent in entries:
+            if query.lower() == ent.lower():
+                return redirect(f"http://127.0.0.1:8000/{ent}")
+            elif query in ent:
+                return render(request, "./encyclopedia/index.html", {"query": query, "entry": ent, "entries": entries})
